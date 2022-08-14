ed310ebc21d059e33e00f4494af79dce20a44e39
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-14 04:06:30 -0700

    test function call from Vue template

--- p1/app.js
+++ p1/app.js
@@ -1,19 +1,32 @@
 "use strict";
 
-Vue.createApp({
+const vm = Vue.createApp({
     data() {
         return {
             firstName: "Thanh",
             lastName: "Nguyen",
         }
-    }
-}).mount(".app-1");
-
-Vue.createApp({
-    data() {
-        return {
-            job: "Programmer",
-            salary: "130000",
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName} ${this.lastName.toUpperCase()}`;
         }
     }
-}).mount(".app-2");
+}).mount("#app-1");
+
+
+// setTimeout(() => {
+//     vm.firstName = "Mike";
+//     vm.lastName = "McNillin";
+// }, 2000);
+
+
+
+// Vue.createApp({
+//     data() {
+//         return {
+//             job: "Programmer",
+//             salary: "130000",
+//         }
+//     }
+// }).mount(".app-2");
