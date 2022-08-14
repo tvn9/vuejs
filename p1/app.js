d6e8a5518a9c4ec14dcb90870be3f0c83c4ec2b6
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-14 12:45:06 -0700

    :value, @input, @click event example

--- p1/app.js
+++ p1/app.js
@@ -7,11 +7,18 @@ const vm = Vue.createApp({
             lastName: "Nguyen",
             url: "https://www.udemy.com/",
             raw_url: '<a href="https://www.udemy.com/" target="_blank">Udemy.com</a>',
+            age: 20,
         }
     },
     methods: {
         fullName() {
-            return `${this.firstName} ${this.lastName.toUpperCase()}`;
+            return `${this.firstName} ${this.lastName.toUpperCase()}`
+        },
+        increment() {
+            this.age++
+        },
+        updateLname(event) {
+            this.lastName = event.target.value
         }
     }
 }).mount("#app-1");
