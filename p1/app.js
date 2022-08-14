38cc1cf5777a3ba138c00063f58d29fa95adf146
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-14 03:39:54 -0700

    Test vue html template

--- p1/app.js
+++ p1/app.js
@@ -1,3 +1,19 @@
 "use strict";
 
-Vue.createApp({}).mount("#app");
+Vue.createApp({
+    data() {
+        return {
+            firstName: "Thanh",
+            lastName: "Nguyen",
+        }
+    }
+}).mount(".app-1");
+
+Vue.createApp({
+    data() {
+        return {
+            job: "Programmer",
+            salary: "130000",
+        }
+    }
+}).mount(".app-2");
