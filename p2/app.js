afe3f9783ad754dee46fc28ad36cfce3e0b4fe30
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-15 01:18:40 -0700

    useing computed properties to togle class

---
+++ p2/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let vm = Vue.createApp({
+    data() {
+        return {
+            isPurple: false
+        }
+    },
+    computed: {
+        circle_classes() {
+            return { purple: this.isPurple }
+        }
+    } 
+}).mount('#app-1');
