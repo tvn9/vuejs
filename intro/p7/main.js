42fb45ae2825350eb21c8d02391cb1669d454e88
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-16 12:04:18 -0700

    Added methods addToCart and updateImage

--- intro/p7/main.js
+++ intro/p7/main.js
@@ -3,6 +3,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Sock",
       image: "./assets/images/socks_green.jpg",
       vueMastery: "https://vuemastery.com",
@@ -10,15 +11,22 @@ const app = Vue.createApp({
       inventory: 19,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2345, color: "Green" },
-        { id: 3235, color: "Blue" },
-        { id: 2333, color: "Yellow" },
+        { id: 2345, color: "Green", image: "./assets/images/socks_green.jpg" },
+        { id: 3235, color: "Blue", image: "./assets/images/socks_blue.jpg" },
       ],
       sizes: [
         { id: 1111, size: "Small" },
         { id: 1112, size: "Medium" },
         { id: 1113, size: "Large" },
-      ],
+      ]
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage
     }
   }
 });
