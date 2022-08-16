7d00089eff91cec6b8e7a5e3ccecfee00663f5b4
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-16 04:23:22 -0700

    Adding vue3 - v-bind example

---
+++ intro/p4/app4.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const app = Vue.createApp({
+  data() {
+    return {
+      taskList: "Finish the course!",
+      vueLink: "https://vuejs.org",
+    };
+  }
+});
+
+app.mount("#event-action");
+
+
+
---
+++ intro/p6/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Sock",
+      image: "./assets/images/socks_green.jpg",
+      vueMastery: "https://vuemastery.com",
+    }
+  }
+})
