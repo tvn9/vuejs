88b4d47efa24df1c3d4fe2012bfb52adb4e317a0
Thanh Nguyen <tv.nguyen9@outlook.com>
2023-04-27 19:14:36 -0700

    adding L7 practice code

--- L7/main.js
+++ L7/main.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
       return {
          cart: 0,
          product: 'Socks',
+         brand: 'Vue Mastery',
          image: './assets/images/socks_blue.jpg',
          inStock: true,
          details: ['50% cotton', '30% wool', '20% polyester'],
@@ -13,16 +14,11 @@ const app = Vue.createApp({
       }
    },
    methods: {
-      addToCard() {
-         return this.cart++
+      addToCart() {
+         this.cart += 1
       },
-      removeFromCard() {
-         if (this.cart > 0) {
-            return this.cart--
-         }
-      },
-      updateImage(image) {
-         return this.image = image
+      updateImage(variantImage) {
+         this.image = variantImage
       }
    }
 })
